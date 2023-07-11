0aa24a55e6baa4f7a498da78e09c57207ff424b1
@stekiefassil <samrawit.a.tekie-fassil@capgemini.com>
2023-07-11 21:00:29 +0200

    Add GET ordered bookings

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -1,13 +1,13 @@
 const sqlDb = require("../utils/database").sqlDb;
 const getDb = require("../utils/database").getDb;
+const { ObjectId } = require("mongodb");
 
-exports.getAllBookingsByLoc = (req, res, next) => {
+exports.getBookingsByLoc = (req, res, next) => {
   const locationId = req.params.locationId;
   let bookingData;
-  let usersData = {};
   sqlDb
     .execute(
-      `SELECT b.id_user, b.booking_date, s.service_type, s.duration, st.name AS staff_name, st.surname AS staff_surname, l.city, l.tot_work_stations FROM bookings AS b
+      `SELECT b.id_user AS user, b.booking_date, s.service_type, s.duration, st.name AS staff_name, st.surname AS staff_surname, l.city, l.tot_work_stations FROM bookings AS b
         INNER JOIN services AS s ON b.id_service = s.id
         INNER JOIN staff AS st ON st.id = b.id_staff
         INNER JOIN locations AS l ON l.id = b.id_location
@@ -16,23 +16,26 @@ exports.getAllBookingsByLoc = (req, res, next) => {
     )
     .then((result) => {
       bookingData = result[0];
-      const userIds = result[0].map((res) => res.id_user);
+      const userIds = result[0].map((res) => new ObjectId(res.user));
       getDb()
         .db()
         .collection("users")
-        .find({})
-        .forEach((userData) => {
-          if (userIds.includes(userData._id.toString())) {
-            usersData[userData._id.toString()] = {
-              userName: userData.userName,
-              name: userData.name,
-              surname: userData.surname,
-            };
+        .find({ _id: { $in: userIds } })
+        .toArray()
+        .then((data) => {
+          console.log({ data });
+          // console.log({
+          //   usersData,
+          //   bookingData,
+          //   userData: bookingData[0].user,
+          // });
+          data.forEach((userData) => {
             bookingData
-              .filter((b) => b.id_user === userData._id.toString())
+              .filter((b) => b.user === userData._id.toString())
               .forEach((currB) => {
-                if (currB.id_user === userData._id.toString()) {
+                if (currB.user === userData._id.toString()) {
                   currB.user = {
+                    id: currB.user,
                     userName: userData.userName,
                     name: userData.name,
                     surname: userData.surname,
@@ -40,14 +43,7 @@ exports.getAllBookingsByLoc = (req, res, next) => {
                 }
                 return currB;
               });
-          }
-        })
-        .then((data) => {
-          // console.log({
-          //   usersData,
-          //   bookingData,
-          //   userData: bookingData[0].user,
-          // });
+          });
           res.status(200).json({ data: { bookingData } });
         });
     })
@@ -56,3 +52,97 @@ exports.getAllBookingsByLoc = (req, res, next) => {
       res.status(500).json({ message: "Failed to fetch bookings." });
     });
 };
+
+exports.getBookingsByUser = (req, res, next) => {
+  const userId = req.params.userId;
+  sqlDb
+    .execute(
+      `SELECT b.id_user, b.booking_date, s.service_type, s.duration, st.name AS staff_name, st.surname AS staff_surname, l.city FROM bookings AS b
+      INNER JOIN services AS s ON b.id_service = s.id
+      INNER JOIN staff AS st ON st.id = b.id_staff
+      INNER JOIN locations AS l ON l.id = b.id_location
+      WHERE b.id_user = ?;`,
+      [userId]
+    )
+    .then((result) => {
+      console.log({ result });
+      res.status(200).json({ data: result[0] });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Failed to fetch user bookings." });
+    });
+};
+
+exports.getBookingsGroupedByMonth = (req, res, next) => {
+  const locationId = req.params.locationId;
+  let bookingData;
+  sqlDb
+    .execute(
+      `SELECT DATE_FORMAT(booking_date, '%m-%Y') AS month_group, 
+        b.id_user AS user, b.booking_date, s.service_type, s.duration, st.name AS staff_name, st.surname AS staff_surname, l.city FROM bookings AS b
+        INNER JOIN services AS s ON b.id_service = s.id
+        INNER JOIN staff AS st ON st.id = b.id_staff
+        INNER JOIN locations AS l ON l.id = b.id_location
+        WHERE b.id_location = ?
+        GROUP BY MONTH(booking_date), YEAR(booking_date), b.id`,
+      [locationId]
+    )
+    .then((result) => {
+      const userIds = result[0].map((res) => new ObjectId(res.user));
+      getDb()
+        .db()
+        .collection("users")
+        .find({ _id: { $in: userIds } })
+        .forEach((user) => {
+          bookingData = result[0].reduce((acc, curr, i) => {
+            const { month_group, ...data } = curr;
+            console.log({ acc, curr, user });
+            acc[month_group] = acc[month_group] || [];
+            curr.user = {
+              id: user._id,
+              userName: user.userName,
+              name: user.name,
+              surname: user.surname,
+            };
+            acc[month_group].push(data);
+            return acc;
+          }, {});
+        })
+        .then((data) => {
+          res.status(200).json({ data: { bookingData } });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Failed to fetch bookings." });
+    });
+};
+
+// exports.getServiceById = (req, res, next) => {
+//   const type = req.params.type;
+//   sqlDb
+//     .execute(`SELECT * FROM services WHERE service_type=?`, [type])
+//     .then((data) => {
+//       console.log({ "service type": data });
+//       return res.status(200).json({ type: data });
+//     });
+// };
+
+// exports.addBooking = (req, res, next) => {
+//   const userId = req.body.userId;
+//   const service = req.body.service;
+//   const location = req.body.location;
+//   let serviceId;
+//   let locationId;
+//   sqlDb
+//     .execute(`SELECT * FROM services WHERE service_type=?`, [service])
+//     .then((data) => {
+//       console.log({ "service type": data });
+//       // return res.status(200).json({ type: data });
+//       serviceId = data.id;
+//     });
+//   console.log({ serviceId });
+//   // const staff = req.body.staff; TODO: assign randomly
+//   const date = new Date();
+// };
--- routes/bookings.js
+++ routes/bookings.js
@@ -5,6 +5,13 @@ const bookingsController = require("../controllers/bookings");
 
 const router = express.Router();
 
-router.get("/bookings/:locationId", bookingsController.getAllBookingsByLoc);
+router.get("/bookings/:locationId", bookingsController.getBookingsByLoc);
+router.get(
+  "/bookings/:locationId/:month",
+  bookingsController.getBookingsGroupedByMonth
+);
+router.get("/user-bookings/:userId", bookingsController.getBookingsByUser);
+
+// router.post("/new-booking", bookingsController.addBooking);
 
 module.exports = router;
